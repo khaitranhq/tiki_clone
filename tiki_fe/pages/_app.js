d5e3bbf54f70145e61a12092cf911a37575c744e
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-01-01 18:20:43 +0700

    completed init material ui

--- tiki_fe/pages/_app.js
+++ tiki_fe/pages/_app.js
@@ -1,8 +1,9 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { Head } from 'next/head';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import theme from '../src/utils/theme';
+import Head from 'next/head';
 
 export default function _App(props) {
   const { Component, pageProps } = props;
@@ -15,15 +16,20 @@ export default function _App(props) {
   }, []);
 
   return (
-    <Fragment>
+    <React.Fragment>
       <Head>
-        <title>Tiki</title>
+        <title>My page</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
       </Head>
       <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </Fragment>
+    </React.Fragment>
   );
 }
 
@@ -31,4 +37,3 @@ _App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired
 };
-
